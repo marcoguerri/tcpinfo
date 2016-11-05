ce3507f0504249706f96a6e837b2fcfad221444c
Marco Guerri <marco.guerri.dev@fastmail.com>
2016-11-05 19:47:43 +0100

    Parametrized the definition of the tcp_info parameters to be printed

--- src/tcpinfo.c
+++ src/tcpinfo.c
@@ -21,6 +21,7 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 #include <dlfcn.h>
 #include <signal.h>
 #include <inttypes.h>
@@ -29,39 +30,138 @@
 #include <netinet/in.h>
 #include <poll.h>
 #include <errno.h>
-
+#include <unistd.h>
+#include <stddef.h>
+#include <stdint.h>
 #include "list.h"
 
-node_t* list_sock = NULL;
+list_node_t* list_sock = NULL;
 
+#define STRLEN(s) (sizeof(s)/sizeof(s[0]))
+#define ENABLE_FIELD(f, fmt) {#f, fmt, offsetof(struct tcp_info, tcpi_##f)} 
+struct tpcinfo_field 
+{
+    /* Name of the field of interest */
+    char *name;
+    /* Format string without % */
+    char fmt[4];
+    /* Offset within tcp_info */
+    unsigned int offset;
+};
+
+struct tpcinfo_field tcpinfo_fields_enabled[] = 
+{
+    ENABLE_FIELD(state, "hhu"),
+    ENABLE_FIELD(ca_state, "hhu"),
+    ENABLE_FIELD(retransmits, "hhu"),
+    ENABLE_FIELD(probes, "hhu"),
+    ENABLE_FIELD(backoff, "hhu"),
+    ENABLE_FIELD(options, "hhu"),
+    ENABLE_FIELD(rto, "u"),
+    ENABLE_FIELD(lost, "u"),
+    ENABLE_FIELD(retrans, "u"),
+    ENABLE_FIELD(total_retrans, "u"),
+    ENABLE_FIELD(advmss, "u"),
+    ENABLE_FIELD(ato, "u"),
+    ENABLE_FIELD(fackets, "u")
+};
+
+
+
+void print_summary_header()
+{
+    uint32_t i = 0;
+    char *fmt;
+    fprintf(stdout, "%5s", "fd");
+    
+    fmt = (char*) malloc(sizeof(char) * 50);
+    if(fmt == NULL) 
+    {
+        perror("malloc");
+        exit(1);
+    }
+
+    for(; i < sizeof(tcpinfo_fields_enabled)/sizeof(tcpinfo_fields_enabled[0]); i++) 
+    {
+        sprintf(fmt, "%%%zus", strlen(tcpinfo_fields_enabled[i].name) + 5);
+        fprintf(stdout, fmt, tcpinfo_fields_enabled[i].name);
+
+    }
+    fprintf(stdout,"\n");
+    free(fmt);
+}
+
+void print_summary_socket(int sock_fd, struct tcp_info* tcpinfo)
+{
+    /*
+     * One remark before everything else: some of these counters are reset
+     * farily often (e.g. retrans or lost), therefore they do not account for
+     * all the actual events. One possible solution is to define a sampling rate
+     * and keep track of a cumulative counter. Of course this would not account
+     * for all the real events either, but would give an idea of the number of
+     * events integrated over time. 
+     * 
+     * Follows an attempt to explain the meaning of each field.
+     * advmss:   the MSS ('Maximal Segment Size') supported by the connection. 
+     *           Linux uses a default value calculated from the first hop device MTU. 
+     * ato:      ACK timeout, timeout period for sending a delayed ack, relevant
+     *           when delaying ACKs in order to group them together. Controller by
+     *           /proc/sys/net/ipv4/tcp_ato_min
+     * backoff:  TCP exponential backoff strategy?
+     * ca_state: Current state of the FSA which controls the TCP congestion algorithm
+     * fackets:  What the hell is this? 
+     *
+     */
+    uint32_t i = 0;
+    char *fmt;
+
+    fprintf(stdout, "%5d", sock_fd);
+    fmt = (char*) malloc(sizeof(char) * 50);
+    for(; i < sizeof(tcpinfo_fields_enabled)/sizeof(tcpinfo_fields_enabled[0]); i++)
+    { 
+        sprintf(fmt, "%%%zu%s", 
+                strlen(tcpinfo_fields_enabled[i].name) + 5,
+                tcpinfo_fields_enabled[i].fmt);
+        uint8_t *ptr_field = (uint8_t*)tcpinfo + tcpinfo_fields_enabled[i].offset;
+        fprintf(stdout, fmt, *((uint32_t*)ptr_field));
+    }
+    fprintf(stdout, "\n");
+    free(fmt);
+
+}
 
 void sigusr_callback(int signum)
 {
 
     struct tcp_info tcp_info;
+    int sock_fd;
+    uint32_t index = 0;
+    
     socklen_t tcp_info_len = sizeof(struct tcp_info);
     if(list_len(list_sock) == 0)
     {
         fprintf(stderr, "No active sockets\n");
         return;
     }
-    int sock_fd = *((int*)list_get(list_sock, 0));
 
-    int ret = getsockopt(sock_fd, 
-                         IPPROTO_TCP,
-                         TCP_INFO,
-                         (void *)&tcp_info,
-                         &tcp_info_len);
-    if(ret == 0) 
+    size_t num_sockets = list_len(list_sock);
+    for(; index < num_sockets; index++ )
     {
-        fprintf(stderr, "Lost: %u, Retransmitted: %u\n",
-                        tcp_info.tcpi_lost,
-                        tcp_info.tcpi_total_retrans);
-    } 
-    else 
-    {
-        fprintf(stderr, "Error querying TCP_INFO\n");
+        sock_fd = *((int*)list_get(list_sock, 0));
+        int ret = getsockopt(sock_fd, 
+                             IPPROTO_TCP,
+                             TCP_INFO,
+                             (void *)&tcp_info,
+                             &tcp_info_len);
+
+        print_summary_header();
+
+        if(ret == 0) 
+            print_summary_socket(sock_fd, &tcp_info);
+        else 
+            fprintf(stderr, "Error querying TCP_INFO\n");
     }
+
 }
 
 int poll(struct pollfd *fds, nfds_t nfds, int timeout)
@@ -83,8 +183,10 @@ int socket(int domain, int type, int protocol)
     struct sigaction sigusr_action;
     sigusr_action.sa_handler = &sigusr_callback;
     /* 
-     * poll is never restarted if interrupted, regardless of SA_RESTART.
-     * If the server side is polling, upon receiving SIGUSR it will will return
+     * Setting handler for SIGUSR1, specifying SA_RESTART to restart syscalls.
+     * In some cases, this is not enough. For instance, poll is never restarted 
+     * if interrupted, regardless of SA_RESTART. If the any of the two ends of the
+     * communication is polling, upon receiving SIGUSR it will will return
      * with EINTR and it will most likely exit. A hook for poll is set to work 
      * around this issue.
      */
@@ -104,9 +206,16 @@ int socket(int domain, int type, int protocol)
         {
             /* Signal handler has not been installed yet */
             if(sigaction(SIGUSR1, &sigusr_action, NULL) < 0)
+            {
                 fprintf(stderr,"Could not install SIGUSR1 signal\n");
-            else
+            }
+            else 
+            {
                 fprintf(stderr,"Signal handler installed correctly\n");
+                pid_t pid = getpid();
+                fprintf(stderr, "Signal PID %d to obtain tcp socket info\n", pid);
+            }
+            
         }
     }
     
@@ -114,8 +223,8 @@ int socket(int domain, int type, int protocol)
     socket_libc = (int(*)(int, int, int))dlsym(RTLD_NEXT, "socket");
     int fd = (*socket_libc)(domain, type, protocol);
     
-    /* Consider only SOCK_STREAM sockets */
-    if(type != SOCK_STREAM)
+    /* Consider only AF_INET and SOCK_STREAM sockets */
+    if( (domain != AF_INET && domain != AF_INET6) ||  type != SOCK_STREAM)
         return fd;
  
     if(list_sock == NULL)
@@ -136,8 +245,8 @@ int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen)
  
     /* Calling accept on socketfd requires having previously called socket(),
      * which initializes the list. If list is indeed NULL, accept is being called
-     * on a socket which has not been initialized or on a socket != SOCK_STREAM,
-     * which should not happen anyway. Do nothing in this case */
+     * on a socket which has not been initialized or on a socket != AF_INET | AF_INET6
+     * and SOCK_STREAM. Do nothing in this case */
 
     if(list_sock != NULL)
     {
