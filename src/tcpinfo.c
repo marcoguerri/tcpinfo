fc763942755a50c01ea35306c338f5a46ffd2d09
Marco Guerri <marco.guerri.dev@fastmail.com>
2017-02-15 22:39:40 +0100

    Added debug macro

--- src/tcpinfo.c
+++ src/tcpinfo.c
@@ -2,21 +2,21 @@
  * The MIT License (MIT)
  * Copyright (C) 2016 Marco Guerri
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of 
- * this software and associated documentation files (the "Software"), to deal in 
- * the Software without restriction, including without limitation the rights to use, 
- * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
  * Software, and to permit persons to whom the Software is furnished to do so, subject
  * to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all 
+ * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
- * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
- * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
@@ -26,6 +26,7 @@
 #include <signal.h>
 #include <inttypes.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <linux/tcp.h>
 #include <netinet/in.h>
 #include <poll.h>
@@ -36,12 +37,20 @@
 #include "list.h"
 #include "config.h"
 
+#ifdef DEBUG
+#define debug(fmt, ...) \
+        fprintf(stderr, fmt, ##__VA_ARGS__)
+#else
+#define debug(fmt, ...) {}
+#endif
+
+
 list_node_t* list_sock = NULL;
 
 #define STRLEN(s) (sizeof(s)/sizeof(s[0]))
-#define ENABLE_FIELD(f, fmt) {#f, fmt, offsetof(struct tcp_info, tcpi_##f)} 
+#define ENABLE_FIELD(f, fmt) {#f, fmt, offsetof(struct tcp_info, tcpi_##f)}
 
-struct tpcinfo_field 
+struct tpcinfo_field
 {
     /* Name of the field of interest */
     char *name;
@@ -57,10 +66,10 @@ struct tcpinfo_fd_pair
 {
     int sock_fd;
     struct tcp_info tcpinfo;
-}; 
+};
 
 
-struct tpcinfo_field tcpinfo_fields_enabled[] = 
+struct tpcinfo_field tcpinfo_fields_enabled[] =
 {
 #ifdef HAVE_STRUCT_TCP_INFO_TCPI_STATE
     ENABLE_FIELD(state, "hhu"),
@@ -123,18 +132,16 @@ void print_summary_sockets(list_node_t* list_tcp_info)
      * all the actual events. One possible solution is to define a sampling rate
      * and keep track of a cumulative counter. Of course this would not account
      * for all the real events either, but would give an idea of the number of
-     * events integrated over time. 
-     * 
+     * events integrated over time.
+     *
      * Follows an attempt to explain the meaning of each field.
-     * advmss:   the MSS ('Maximal Segment Size') supported by the connection. 
-     *           Linux uses a default value calculated from the first hop device MTU. 
+     * advmss:   The MSS ("Maximal Segment Size") supported by the connection.
+     *           Linux uses a default value calculated from the first hop device MTU.
      * ato:      ACK timeout, timeout period for sending a delayed ack, relevant
      *           when delaying ACKs in order to group them together. Controller by
      *           /proc/sys/net/ipv4/tcp_ato_min
      * backoff:  TCP exponential backoff strategy?
      * ca_state: Current state of the FSA which controls the TCP congestion algorithm
-     * fackets:  What the hell is this? 
-     *
      */
     uint32_t i = 0, j = 0;
     char fmt[10];
@@ -147,9 +154,9 @@ void print_summary_sockets(list_node_t* list_tcp_info)
         fprintf(stdout, "%15d", p->sock_fd);
     }
     fprintf(stdout, "\n");
-    
+
     for(i = 0; i < sizeof(tcpinfo_fields_enabled)/sizeof(tcpinfo_fields_enabled[0]); i++)
-    { 
+    {
         fprintf(stdout, "%15s", tcpinfo_fields_enabled[i].name);
         for(j = 0; j < list_len(list_tcp_info); ++j)
         {
@@ -176,16 +183,16 @@ void sigusr_callback(int signum)
 
     socklen_t tcp_info_len = sizeof(struct tcp_info);
     size_t num_sockets = list_len(list_sock);
-    
+
     for(i = 0; i < num_sockets; ++i )
     {
         sock_fd = *((int*)list_get(list_sock, 0));
-        int ret = getsockopt(sock_fd, IPPROTO_TCP, TCP_INFO, 
+        int ret = getsockopt(sock_fd, IPPROTO_TCP, TCP_INFO,
                             (void *)&tcp_info, &tcp_info_len);
-        
+
         if(ret < 0)
             continue;
-            
+
         /* Snapshot of tcp_info structure obtained from the kernel */
         memcpy(&p.tcpinfo, &tcp_info, sizeof(struct tcp_info));
         p.sock_fd = sock_fd;
@@ -194,9 +201,9 @@ void sigusr_callback(int signum)
         if(list_tcp_info == NULL)
             list_tcp_info = list_init(&p, sizeof(struct tcpinfo_fd_pair));
         else
-            list_tcp_info = list_insert(list_tcp_info, &p, 
-                                        sizeof(struct tcpinfo_fd_pair), 
-                                        list_len(list_tcp_info)); 
+            list_tcp_info = list_insert(list_tcp_info, &p,
+                                        sizeof(struct tcpinfo_fd_pair),
+                                        list_len(list_tcp_info));
 
     }
     print_summary_sockets(list_tcp_info);
@@ -205,17 +212,15 @@ void sigusr_callback(int signum)
 
 int poll(struct pollfd *fds, nfds_t nfds, int timeout)
 {
-    /* Hook for poll which restarts the system call upon receiving a signal.
+    /* poll hook which restarts the system call upon receiving a signal.
      * poll normally returns always EINTR when interrupted even if SA_RESTART
-     * is set. The following code will restart poll until the return value is != -1
-     * (even though it's very unlikely that poll is interrupted twice during
-     * the same invocation) */
-    
+     * is set. The this will restart poll until some != from EINTR is returned */
+
     int (*poll_libc)(struct pollfd*, nfds_t, int);
     poll_libc = (int(*)(struct pollfd *, nfds_t, int))dlsym(RTLD_NEXT, "poll");
-    
+
     int ret = (*poll_libc)(fds, nfds, timeout);
-    while(ret == -1 && errno == EINTR) 
+    while(ret == -1 && errno == EINTR)
         ret = (*poll_libc)(fds, nfds, timeout);
 
     return ret;
@@ -225,67 +230,71 @@ int socket(int domain, int type, int protocol)
 {
     struct sigaction sigusr_action;
     sigusr_action.sa_handler = &sigusr_callback;
-    /* 
+    /*
      * Setting a handler for SIGUSR1, specifying SA_RESTART to restart syscalls.
-     * In some cases, this is not enough. For instance, poll is never restarted 
+     * In some cases, this is not enough. For instance, poll is never restarted
      * if interrupted, regardless of SA_RESTART. If the any of the two ends of the
-     * communication is polling, upon receiving SIGUSR it will will return
-     * with EINTR and it will most likely exit. A hook for poll is set to work 
-     * around this issue.
+     * communication is polling, upon receiving SIGUSR it will return
+     * with EINTR and it will most likely exit. A hook for poll is set to restart
+     * the call when EINTR is returned.
      */
     sigusr_action.sa_flags = SA_RESTART;
-   
-    struct sigaction old_action; 
+
+    struct sigaction old_action;
     int ret = sigaction(SIGUSR1, NULL, &old_action);
-    if(ret < 0) 
+    if(ret < 0)
     {
         /* Cannot verify if the signal is installed. Forcing installation */
         if(sigaction(SIGUSR1, &sigusr_action, NULL) < 0)
-            fprintf(stderr,"Could not install SIGUSR1 signal\n");
+            debug("Could not install SIGUSR1 signal\n");
     }
     else
     {
-        if(old_action.sa_handler != &sigusr_callback)    
+        if(old_action.sa_handler != &sigusr_callback)
         {
             /* Signal handler has not been installed yet */
             if(sigaction(SIGUSR1, &sigusr_action, NULL) < 0)
             {
-                fprintf(stderr,"Could not install SIGUSR1 signal\n");
+                debug("Could not install SIGUSR1 signal\n");
             }
-            else 
+            else
             {
-                fprintf(stderr,"Signal handler installed correctly\n");
+                debug("Signal handler installed correctly\n");
                 pid_t pid = getpid();
-                fprintf(stderr, "Signal PID %d to obtain tcp socket info\n", pid);
+                debug( "Signal PID %d to obtain tcp socket info\n", pid);
             }
-            
+
         }
     }
-    
+
     int (*socket_libc)(int, int, int);
     socket_libc = (int(*)(int, int, int))dlsym(RTLD_NEXT, "socket");
     int fd = (*socket_libc)(domain, type, protocol);
-    
+
     /* Consider only AF_INET and SOCK_STREAM sockets */
     if( (domain != AF_INET && domain != AF_INET6) ||  type != SOCK_STREAM)
+    {
+        debug("Ignoring socker %d\n", fd);
         return fd;
- 
+    }
+
+    debug("Adding socket %d\n", fd);
     if(list_sock == NULL)
         list_sock = list_init(&fd, sizeof(int));
     else
-        list_sock = list_insert(list_sock, &fd, sizeof(int), list_len(list_sock)); 
+        list_sock = list_insert(list_sock, &fd, sizeof(int), list_len(list_sock));
 
     return fd;
 }
 
 int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen)
 {
-    
+
     int (*accept_libc)(int, struct sockaddr*, socklen_t*);
     accept_libc = (int(*)(int, struct sockaddr*, socklen_t*))dlsym(RTLD_NEXT, "accept");
-    
+
     int fd = (*accept_libc)(sockfd, addr, addrlen);
- 
+
     /* Calling accept on socketfd requires having previously called socket(),
      * which initializes the list. If list is indeed NULL, accept is being called
      * on a socket which has not been initialized or on a socket != AF_INET | AF_INET6
@@ -293,15 +302,16 @@ int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen)
 
     if(list_sock != NULL)
     {
-        if(list_search(list_sock, &sockfd, sizeof(int)) == NULL) 
+        if(list_search(list_sock, &sockfd, sizeof(int)) == NULL)
         {
             /* Welcoming socket must be already in the list. If not, warn but
              * add the new socket anyway */
-            fprintf(stderr, "Welcoming socket is not in the list?\n");
+            debug( "Welcoming socket is not in the list?\n");
         }
-        list_sock = list_insert(list_sock, &fd, sizeof(int), list_len(list_sock)); 
+        debug("Adding socket %d\n", fd);
+        list_sock = list_insert(list_sock, &fd, sizeof(int), list_len(list_sock));
     }
- 
+
     return fd;
 }
 
